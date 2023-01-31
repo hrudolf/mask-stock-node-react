02fdc1d8d05da37361d8fd937443fe4322698d3d
Peterfug <fugedi.peter5@gmail.com>
2023-01-31 17:27:33 +0100

    User profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import Register from "./components/Register";
 import { Routes, Route } from 'react-router-dom'
+import Profile from "./components/UserProfile";
+
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -19,8 +21,9 @@ function App() {
         }
 =======
         {/* {/* <Route path="/login" element={<Login user={loggedUser} />}></Route> */}
-        <Route path="/register" element={<Register />}></Route> */
->>>>>>> 3587afee74425653482ff2df499cd1c9059e8995
+        <Route path="/profile" element={<Profile  user={loggedUser}/>}></Route>
+        <Route path="/register" element={<Register />}></Route>
+
       </Routes>
     </div>
   );
