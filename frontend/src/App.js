1d6d1ea45539191966cce18102ed43d4eb6eff95
hrudolf <h.rudolf@gmail.com>
2023-01-31 14:14:12 +0100

    Comment out login element

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Nav loggedIn={loggedIn} setLoggedIn={setLoggedIn} loggedUser={loggedUser} setLoggedUser={setLoggedUser} />
       <Routes>
         <Route path="/" element={<Home user={loggedUser} />}></Route>
-        <Route path="/login" element={<Login user={loggedUser} />}></Route>
+        {/* <Route path="/login" element={<Login user={loggedUser} />}></Route> */}
       </Routes>
     </div>
   );
